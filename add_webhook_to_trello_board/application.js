b7c13645ab6bfd241439075960ad1b2553843288
Eito Katagiri <eitoball@gmail.com>
2014-07-13 16:38:09 +0900

    add/remove webhooks

--- add_webhook_to_trello_board/application.js
+++ add_webhook_to_trello_board/application.js
@@ -1,3 +1,33 @@
+var get_webhooks = function() {
+  Trello.get("tokens/" + Trello.token() + "/webhooks",
+    function(webhooks) {
+      var body = $("#webhooks tbody");
+      body.empty();
+      $.each(webhooks, function(idx, webhook) {
+        var row = $("<tr>").appendTo(body);
+        var revoke = $("<button>").addClass("btn btn-danger").text("Revoke").attr("data-webhookId", webhook.id);
+        revoke.click(function(e) {
+          if (confirm("Revoke?")) {
+            Trello.delete("tokens/" + Trello.token() + "/webhooks/" + $(this).data().webhookid,
+                          function() { alert("Revoked"); get_webhooks(); },
+                          function() { alert("Failed to revoke"); get_webhooks(); });
+          }
+        });
+        Trello.boards.get(
+          webhook.idModel,
+          function(board) {
+            $("<td>").text(board.name).appendTo(row);
+            $("<td>").text(webhook.callbackURL).appendTo(row);
+            $("<td>").text(webhook.description).appendTo(row);
+            var item = $("<td>").appendTo(row);
+            revoke.appendTo(item);
+          },
+          function() { });
+      });
+    },
+    function() { });
+};
+
 var onAuthorized = function() {
   $("#authorized").show();
   $("#not-authorized").hide();
@@ -7,17 +37,27 @@ var onAuthorized = function() {
       $("<option>").val(board.id).text(board.name).appendTo(select_boards);
     });
   });
-  Trello.get("tokens/" + Trello.token() + "/webhooks",
-    function(webhooks) {
-      $.each(webhooks, function(idx, webhook) {
-        alert(webhook.callbackURL);
-      });
-    },
-    function() { });
+  get_webhooks();
 };
+
 var onDenied = function() {
   alert("error");
 };
+
+var add_webhook = function(board_id, callback_url) {
+  Trello.post("tokens/" + Trello.token() + "/webhooks", {
+      description: "idobata",
+      callbackURL: callback_url,
+      idModel: board_id
+    },
+    function() {
+      alert("Added");
+      get_webhooks();
+    },
+    function() { alert("Failed"); }
+  );
+};
+
 $(document).ready(function() {
     $("#authorized").hide();
     $("#not-authorized").show();
@@ -31,24 +71,23 @@ $(document).ready(function() {
       });
     });
 
+    $("#spinner").ajaxStart(function() { $(this).spin(); });
+    $("#spinner").ajaxComplete(function() {
+      $(this).spin(false);
+    });
+
     $("#add_webhook").submit(function(e) {
-      if (Trello.authorized()) {
-        var webhook_uri = $("#webhook_uri").val();
-        var board_id = $("#boards option:selected").val();
-        // TODO: check
-        Trello.post("tokens/" + Trello.token() + "/webhooks", {
-            description: "idobata",
-            callbackURL: webhook_uri,
-            idModel: board_id
-          },
-          function() {
-            alert("Added");
-          },
-          function() {
-            alert("Failed");
-          }
-        );
-      }
       e.preventDefault();
+      if (Trello.authorized()) {
+        var board_id = $("#boards option:selected").val();
+        var webhook_uri = $("#webhook_uri").val();
+        if (! board_id) {
+          alert("Select board");
+        } else if (! webhook_uri) {
+          alert("Enter webhook URI");
+        } else {
+          add_webhook(board_id, webhook_uri);
+        }
+      }
     });
 });
